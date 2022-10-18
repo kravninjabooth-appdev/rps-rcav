92a3c0589dd5ef1165cb0a2622a3ee6928c2a465
kravninja <tita0@chicagobooth.edu>
2022-10-18 23:49:05 +0000

    Updated to include wikipedia page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,9 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+    def play_rock
+      #write a ton of code
+      redirect_to("https://www.wikipedia.org")
+    end
+
 end
