c4034d19ae1b5d5c937a795f085594792638c746
kravninja <tita0@chicagobooth.edu>
2022-10-19 01:16:10 +0000

    Added comp_move to paper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,20 @@ class ApplicationController < ActionController::Base
   end
 
   def play_paper
+     comp_move = ["rock", "paper", "scissors"].sample 
+    
+     if comp_move =="rock"
+      outcome = "won"
+     elsif comp_move == "paper" 
+      outcome = "tied"
+     elsif comp_move == "scissors"
+      outcome = "lost"
+     end 
+
+
+
     render ({:template => "game_templates/user_paper.html.erb"})
+
   end
 
 
